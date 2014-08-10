90c48d95a373513a2f3d33eeb1ce8c8d15392957
Javier <arnaix@gmail.com>
2014-08-10 23:43:32 +0200

    fixed pom jacoco includes and deleted unused methods from Success class

--- src/main/java/com/upplication/thepunisher/PunishmentController.java
+++ src/main/java/com/upplication/thepunisher/PunishmentController.java
@@ -46,32 +46,16 @@ class PunishmentController {
             return success;
         }
 
-        public void setSuccess(boolean success) {
-            this.success = success;
-        }
-
         public String getTitle() {
             return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
         }
 
         public String getDescription() {
             return description;
         }
 
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
         public int getId() {
             return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
         }
     }
 
