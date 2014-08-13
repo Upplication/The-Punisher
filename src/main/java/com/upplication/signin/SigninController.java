1970e3b88b505328837715a97906307049a62836
Juan Sicilia Martinez <juan.sicilia@upplication.com>
2014-08-13 18:12:53 +0200

    deleted unused test

--- src/main/java/com/upplication/signin/SigninController.java
+++ src/main/java/com/upplication/signin/SigninController.java
@@ -1,6 +1,8 @@
 package com.upplication.signin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
