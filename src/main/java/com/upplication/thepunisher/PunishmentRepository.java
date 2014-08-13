2266d23fcade8120d619866713e4fd13651db9ba
Juan Sicilia Martinez <juan.sicilia@upplication.com>
2014-08-13 17:20:36 +0200

    First commit
    It`s my first commit

--- src/main/java/com/upplication/thepunisher/PunishmentRepository.java
+++ src/main/java/com/upplication/thepunisher/PunishmentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 @Repository
@@ -16,14 +17,37 @@ public class PunishmentRepository {
 
     @Transactional
     public Punishment create(String title, String description) {
+        if(!(title == "" || description == "" || title.equals("") || description.equals(""))){
+            try{
+                entityManager.createQuery("SELECT p FROM Punishment p WHERE p.title = :title")
+                        .setParameter("title", title)
+                        .getSingleResult();
+            }catch(NoResultException e){
+                Punishment punish = new Punishment();
+                punish.setTitle(title);
+                punish.setDescription(description);
+                entityManager.persist(punish);
+                return punish;
+            }
+        }
+        return null;
+    }
 
-        Punishment punish = new Punishment();
+    @Transactional
+    public boolean remove(int id) {
+        try {
+            Punishment punishment = (Punishment)entityManager.createQuery("SELECT p FROM Punishment p WHERE p.id = :id")
+                    .setParameter("id", id)
+                    .getSingleResult();
+            entityManager.remove(punishment);
+            return true;
+        } catch (NoResultException e) {
+            return false;
+        }
+    }
 
-        punish.setTitle(title);
-        punish.setDescription(description);
-
-        entityManager.persist(punish);
-
-        return punish;
+    @Transactional
+    public Punishment  update(Punishment editPunishment){
+        return entityManager.merge(editPunishment);
     }
 }
--- src/test/java/com/upplication/thepunisher/PunishmentRepositoryIT.java
+++ src/test/java/com/upplication/thepunisher/PunishmentRepositoryIT.java
@@ -63,4 +63,94 @@ public class PunishmentRepositoryIT {
         assertEquals(punishmentBD.getTitle(), punishment.getTitle());
         assertEquals(punishmentBD.getDescription(), punishment.getDescription());
     }
+
+    @Test
+    public void remove_punishment(){
+
+        final String title = "removeMe";
+        final String description = "I wanna be removed";
+
+        Punishment punishment = punishmentRepository.create(title, description);
+
+        Punishment punishmentBD = entityManager.find(Punishment.class, punishment.getId());
+
+        assertNotNull(punishmentBD);
+        assertEquals(punishmentBD.getId(), punishment.getId());
+        assertEquals(punishmentBD.getTitle(), punishment.getTitle());
+        assertEquals(punishmentBD.getDescription(), punishment.getDescription());
+
+
+        boolean ret = punishmentRepository.remove(punishmentBD.getId());
+        assertEquals(ret, true);
+
+    }
+
+    @Test
+    public void edit_punishment(){
+
+        final String title = "editMe";
+        final String description = "I wanna be edited";
+
+        Punishment punishment = punishmentRepository.create(title, description);
+
+        Punishment punishmentBD = entityManager.find(Punishment.class, punishment.getId());
+
+        assertNotNull(punishmentBD);
+        assertEquals(punishmentBD.getId(), punishment.getId());
+        assertEquals(punishmentBD.getTitle(), punishment.getTitle());
+        assertEquals(punishmentBD.getDescription(), punishment.getDescription());
+
+        Punishment editPunishment = new Punishment();
+        editPunishment.setId(punishmentBD.getId());
+        editPunishment.setDescription("I am a edited punishment");
+        editPunishment.setTitle("Edited");
+
+
+        Punishment ret = punishmentRepository.update(editPunishment);
+        assertNotNull(ret);
+        assertEquals(ret.getId(), editPunishment.getId());
+        assertEquals(ret.getTitle(), editPunishment.getTitle());
+        assertEquals(ret.getDescription(), editPunishment.getDescription());
+
+    }
+
+
+    @Test
+    public void repeated_punishment(){
+
+        final String titleOnce = "I appear Twice";
+        final String descriptionOnce = "I wanna appear once";
+
+        punishmentRepository.create(titleOnce, descriptionOnce);
+
+        final String titleTwice = "I appear Twice";
+        final String descriptionTwice = "I wanna appear Twice";
+
+        Punishment punishmentTwice = punishmentRepository.create(titleTwice, descriptionTwice);
+
+        assertEquals(punishmentTwice,null);
+    }
+
+    @Test
+    public void title_empty_punishment(){
+
+        final String titleOnce = "";
+        final String descriptionOnce = "I haven't title";
+
+        Punishment punishmentBD = punishmentRepository.create(titleOnce, descriptionOnce);
+
+        assertEquals(punishmentBD,null);
+    }
+
+    @Test
+    public void description_empty_punishment(){
+
+        final String titleOnce = "I haven't description";
+        final String descriptionOnce = "";
+
+        Punishment punishmentBD = punishmentRepository.create(titleOnce, descriptionOnce);
+
+        assertEquals(punishmentBD,null);
+    }
+
 }
